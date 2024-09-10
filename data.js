3b3f7b46be69a5452343211a089db53a9b59e0d7
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-09-10 14:07:25 +0200

    Added icons to use on webpage, and wrote js functions to get data from API using async/await and to render the content on the webpage

--- data.js
+++ data.js
@@ -1,7 +1,6 @@
 async function getWeatherData (location) {
     let url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + location + '/next7days?unitGroup=metric&key=P9N6HSPKVMA4GG5N3KZBVF9YB&contentType=json';
     const locationData = await getData(url);
-    console.log(locationData)
     return locationData;
 }
 
@@ -16,10 +15,15 @@ async function getData(urlString) {
         const data = await response.json();
         //Retrieving only necessary information
         const adress = data.resolvedAddress;
-        const current = [data.currentConditions.conditions, data.currentConditions.icon, data.currentConditions.temp, data.currentConditions.uvindex];
-        const next7Days = data.days;
+        const current = [
+            data.currentConditions.conditions, 
+            data.currentConditions.icon, 
+            data.currentConditions.temp, 
+            data.currentConditions.precip,
+            data.currentConditions.windspeed
+        ];
         const nextDescription = data.description;
-        return {adress, current, next7Days, nextDescription};
+        return {adress, current, nextDescription};
         }catch(error){
             console.log(`Fetch error: `, error);
                     alert(error);
@@ -32,7 +36,33 @@ const searchField = document.querySelector('#keyword');
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
     const location = searchField.value;
-    getWeatherData(location);
+    renderWeatherReport(location);
 })
 
-getWeatherData('Volda');
+async function renderWeatherReport(location) {
+    const data = await getWeatherData(location);
+    console.log(data);
+    const currentAdress = document.querySelector('#adress');
+    currentAdress.textContent = data.adress;
+
+    const weatherIcon = document.querySelector('#icon');
+    const weatherDescription = document.querySelector('#description');
+    weatherIcon.src = 'Icons/'+ data.current[1] + '.png';
+    weatherDescription.textContent = data.current[0];
+
+    const temp = document.querySelector('#temp');
+    temp.textContent = data.current[2] + ' C';
+
+    const precip = document.querySelector('#precip');
+    precip.textContent = data.current[3] + ' mm';
+    
+    const wind = document.querySelector('#wind');
+    wind.textContent = data.current[4] + ' m/s';
+
+    const nextDays = document.querySelector('#nextDescription');
+    nextDays.textContent = data.nextDescription;
+
+} 
+
+//Initial render
+renderWeatherReport('Volda');
